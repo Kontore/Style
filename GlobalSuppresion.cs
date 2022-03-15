3891b1e1adb0a13e374bbdd4ebf76252f2da9070
tigerros <tigerros.gh@gmail.com>
2022-03-15 22:13:49 +0100

    Create stylecop.json and GlobalSuppresion.cs
    Documentation is being worked on

---
+++ GlobalSuppresion.cs
@@ -0,0 +1,10 @@
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1027:Use tabs correctly", Justification = "Tabs take up less memory and are easier to insert + delete than spaces")]
+[assembly: SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1500:Braces for multi	-line statements should not share line", Justification = "Braces should share line")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1636:File header copyright text should match", Justification = "Should be edit to edit file summary")]
